8be7696002eedbc82445ea2ac79c86ab704bab14
Brayden DeLoach <brayden@deloach.us>
2020-03-25 14:52:52 -0500

    Filled out the contact component.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,35 @@
 import React from "react";
 
 function Contact(){
-    return (<div>contact</div>);
+    return (
+        <div class="container">
+
+        <br/>
+  
+
+        <div class="row justify-content-start">
+  
+          <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+  
+
+            <div class="card border">
+              <div class="card-body">
+                
+                  <h2 class="tc-teal">Contact</h2>
+  
+                  <hr/>
+                  <p><b>Email - </b>brayden@deloach.us</p>
+                  <p><b>Github - </b><a href="https://github.com/deloabra">https://github.com/deloabra</a></p>
+                  <p><b>LinkedIn - </b><a href="https://www.linkedin.com/in/brayden-deloach-666746172/">https://www.linkedin.com/in/brayden-deloach-666746172/</a></p>
+                  <p><b>Resume - </b><a href="assets/Resume.pdf">Resume</a></p>
+            </div>
+  
+          </div>
+        </div>
+  
+      </div>
+      </div>
+    );
 }
 
 export default Contact;
