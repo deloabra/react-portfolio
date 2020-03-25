f0178fef3f9ba30a5e7d893c4d3cf15ec43171bb
Brayden DeLoach <brayden@deloach.us>
2020-03-25 15:29:13 -0500

    Updates

---
+++ precache-manifest.d7fab8261ee67786cebddcdaf9ed0e95.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8e1206bc9479ecdde447add72419f7",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "2264c0601a3b23f39a8d",
+    "url": "/react-portfolio/static/css/main.2fbc4db0.chunk.css"
+  },
+  {
+    "revision": "cc284955e70bae1fe42d",
+    "url": "/react-portfolio/static/js/2.bf5c192f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.bf5c192f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2264c0601a3b23f39a8d",
+    "url": "/react-portfolio/static/js/main.4c38b708.chunk.js"
+  },
+  {
+    "revision": "0346f5e54be923396aed",
+    "url": "/react-portfolio/static/js/runtime-main.8a7d631b.js"
+  },
+  {
+    "revision": "4736be246e435a69cbc98ea715a3fc11",
+    "url": "/react-portfolio/static/media/Quiz.4736be24.gif"
+  },
+  {
+    "revision": "67ef53cd589cb4759fa99ea9f31e671b",
+    "url": "/react-portfolio/static/media/Resume.67ef53cd.pdf"
+  },
+  {
+    "revision": "c4350b2d1f9de320c372695013c9750e",
+    "url": "/react-portfolio/static/media/WellnessJourney.c4350b2d.png"
+  },
+  {
+    "revision": "bc464889410ac890dc2b63655e1520e4",
+    "url": "/react-portfolio/static/media/brayden.bc464889.jpeg"
+  },
+  {
+    "revision": "2fa67277358536f767675213055f956b",
+    "url": "/react-portfolio/static/media/ratPic.2fa67277.png"
+  },
+  {
+    "revision": "f3a9a5c797fc054f9b21e905210659e2",
+    "url": "/react-portfolio/static/media/weatherPic.f3a9a5c7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.202127992668d4a811f9ac6e4fbe1c7f.js"
+  "/react-portfolio/precache-manifest.d7fab8261ee67786cebddcdaf9ed0e95.js"
 );
 
 self.addEventListener('message', (event) => {
