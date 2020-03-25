44b0ea963091ff981ab33af7634b06417e8ff616
Brayden DeLoach <brayden@deloach.us>
2020-03-25 14:13:16 -0500

    Finished skeleton for routes.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
@@ -11,8 +14,10 @@ function App() {
         <Header/>
 
         {/* Routes */}
-        <Route/>
-        
+        <Route exact path="/react-portfolio" component={About}/>
+        <Route exact path="/react-portfolio/contact" component={Contact}/>
+        <Route exact path="/react-portfolio/portfolio" component={Portfolio}/>
+
       </Router>
     </div>
   );
